6641cf250784e2cf6cd12329f350d38da7a8b879
juliennpanev <83950947+juliennpanev@users.noreply.github.com>
2022-08-23 07:31:03 +0300

    Update autoTsunami.py

--- autoTsunami.py
+++ autoTsunami.py
@@ -9,39 +9,45 @@ pw.setNode(node)
 usdnId = 'DG2xFkPdDwKUoBkzGAhQtLpSGzfXLiCYPEzeKH2Ad24p'
 tsunamiContract = '3PNeboTZQz1XhwDY9V9N3K7xYJrq9XbaS5Z'
 tsunami = Tsunami(tsunamiContract, address, node, usdnId)
+
+# Check for open positions
 position = tsunami.getPosition(address.address)
+
+# Set default values
 entryPrice = 0
 invested = 0
-fundingTimestamp = tsunami.getNextFundingTimestamp()
-# funding = tsunami.getTimeToNextFunding()
-payout = tsunami.getPayout(address.address)
-data = tsunami.getDataFromAddress(address.address, 'k_positionOpenNotional')
+
+# Set default values for putting an order
 singleOrderAmount = 10
 margin = 3
 
 while True:
+    # Get markets price
     oraclePrice = tsunami.getOracleTwapPrice()
     marketPrice = tsunami.getTwapSpotPrice()
-    priceDif = marketPrice / oraclePrice
-    p = tsunami.getPositionNotionalAndUnrealizedPnl(address.address)
 
-    if priceDif >= 1.03 and marketPrice > oraclePrice and position['positionSize'] == 0:
+    # Calculate price difference 
+    priceDif = marketPrice / oraclePrice
+
+
+    if priceDif >= 1.03 and position['positionSize'] == 0:
         tsunami.short(singleOrderAmount, margin)
         invested += singleOrderAmount
         entryPrice = marketPrice
+
     if position['margin'] != 0 and invested == 0:
         invested = (position['margin'] * 1.01) / pow(10, 6)
 
 
     payout = tsunami.getPayout(address.address)
-    profit = entryPrice / marketPrice
+    profit = ((invested * margin) * 0.035) + invested
 
-    if payout >= (invested * margin) * 1.03:
+    if payout >= profit:
         tsunami.closePosition()
 
-    if marketPrice >= entryPrice * 1.06:
-        tsunami.short(10, 3)
-        invested += 10
+    if marketPrice >= entryPrice * 1.05:
+        tsunami.short(singleOrderAmount, 3)
+        invested += singleOrderAmount
 
     time.sleep(30)
 
