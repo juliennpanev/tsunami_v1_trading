800f37513e4607c83d4c0c83f205f70177a21a8f
juliennpanev <83950947+juliennpanev@users.noreply.github.com>
2022-10-07 13:42:04 +0300

    20221007

--- readme.txt
+++
@@ -1,1 +0,0 @@
-To run the trading script both files need to be in the same directory!
--- tsunami-pluto-long-F/tsunami-pluto.py
+++ tsunami-pluto-long-F/tsunami-pluto.py
@@ -1,45 +1,57 @@
+from datetime import datetime
 import requests
 import time
 import pywaves as pw
 from tsunami import Tsunami
+import json
 
-node = 'https://nodes.wavesnodes.com'
+
+def printTime():
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    print(dt_string)
+
+
+node = 'http://20.7.14.174:6869'
 address = pw.Address(privateKey='')
 
 pw.setNode(node)
 usdnId = 'DG2xFkPdDwKUoBkzGAhQtLpSGzfXLiCYPEzeKH2Ad24p'
-tsunamiContract = '3PNeboTZQz1XhwDY9V9N3K7xYJrq9XbaS5Z'
+tsunamiContract = '3PA51qCGL57rBWuD7CBTb3NeRQUxwUf6YRp'
 tsunami = Tsunami(tsunamiContract, address, node, usdnId)
 # Set default values
 entryPrice = 0
 invested = 0
 # Set default values for putting an order
-singleOrderAmount = 10
-margin = 4
-
-
+singleOrderAmount = 100
+margin = 3
 
 while True:
     # Check for open positions
     try:
         position = tsunami.getPosition(address.address)
-    except (AtributeError, requests.exceptions.JSONDecodeError, KeyError) as error:
+        oraclePrice = tsunami.getOracleTwapPrice()
+        marketPrice = tsunami.getTwapSpotPrice()
+    except (json.decoder.JSONDecodeError, requests.exceptions.JSONDecodeError,
+            KeyError) as error:
         time.sleep(5)
         continue
 
     # Get markets price
-    oraclePrice = tsunami.getOracleTwapPrice()
-    marketPrice = tsunami.getTwapSpotPrice()
-    # Calculate price difference 
-    priceDif = marketPrice / oraclePrice
-    # If market price > 2% oracle price open short position
-    if priceDif >= 1.03 and position['positionSize'] == 0:
-        tsunami.short(singleOrderAmount, margin)
+
+    # Calculate price difference
+    # priceDif = marketPrice / oraclePrice
+    #invested = (position['margin'] / pow(10, 6)) * 1.01
+    # open LONG position
+    if position['positionSize'] == 0:
+        first_order = tsunami.long(singleOrderAmount, margin)
+        printTime()
+        print(first_order)
         invested += singleOrderAmount
         entryPrice = marketPrice
-        time.sleep(30)
+        time.sleep(10)
         continue
-    if position['margin'] != 0 and invested == 0:
+    if position['margin'] != 0: # and invested == 0:
         invested = (position['margin'] * 1.01) / pow(10, 6)
         size = position['positionSize']
         fundsWithMargin = (position['margin'] * margin)
@@ -50,16 +62,20 @@ while True:
     except:
         time.sleep(5)
         continue
-    profit = ((invested * margin) * 0.03) + invested
-    # If USDN for withdraw covers 3% gain of traded funds CLOSE SHORT
+    profit = ((invested * margin) * 0.025) + invested
+    # If USDN for withdraw covers 1% gain of traded funds CLOSE LONG
 
     if payout >= profit:
         tsunami.closePosition()
+        invested = 0
+        entryPrice = 0
     # if price goes up with 5% when short position opened increase current position / add margin
-    if marketPrice >= entryPrice * 1.03:
-        tsunami.short(singleOrderAmount, margin)
+    if marketPrice <= entryPrice * 0.985:
+        order = tsunami.long(singleOrderAmount, margin)
+        print(order)
+        printTime()
         invested += singleOrderAmount
         entryPrice = marketPrice
 
     # Time interval between checking price changes in seconds
-    time.sleep(5)
+    time.sleep(3)
